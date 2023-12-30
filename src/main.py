28e749e024abeaacf2b9246a5a4a59cf295251de
Makoto Ito <poteto1212@ASUSVivobook>
2023-12-31 01:03:02 +0900

    postを受け取れるようにした

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import predict
+
+
+class Body(BaseModel):
+  bw :float
+  fbs :float
+  glc_3_auc :float
+  glc_plasma_auc :float
+  glc_css :float
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  x_data_list = [1.11,360,1246,124,442]
+@app.post("/predict/")
+def read_root(body: Body):
+  x_data_list = [body.bw,
+                 body.fbs,
+                 body.glc_3_auc,
+                 body.glc_plasma_auc,
+                 body.glc_css]
   y_data_answer=predict.Predict.df_predict_logistic(x_data_list)
   return {"Hello": str(y_data_answer)}
