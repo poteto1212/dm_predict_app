d1d463397e6b995626703f3915021ff4a50fd389
Makoto Ito <poteto1212@ASUSVivobook>
2024-01-01 22:05:35 +0900

    pychacheファイルの除去

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import predict
 
-
 class Body(BaseModel):
   bw :float
   fbs :float
   glc_3_auc :float
   glc_plasma_auc :float
   glc_css :float
+  
 app = FastAPI()
 
 @app.post("/predict/")
@@ -19,4 +19,5 @@ def read_root(body: Body):
                  body.glc_plasma_auc,
                  body.glc_css]
   y_data_answer=predict.Predict.df_predict_logistic(x_data_list)
-  return {"Hello": str(y_data_answer)}
+  
+  return {"Preduct": str(y_data_answer)}
