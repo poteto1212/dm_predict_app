36e8eab627fe53362e4b1092ec2ec501cf05d8fc
Makoto Ito <poteto1212@ASUSVivobook>
2024-01-25 20:46:33 +0900

    DB接続情報の環境変数化

--- src/db/alembic/env.py
+++ src/db/alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -63,6 +63,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    config.set_section_option("alembic", "MYSQL_URL", os.environ.get("MYSQL_URL"))
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,7 @@
 #データベース関連の設定
 from sqlalchemy import create_engine
+import os
 
-url = "mysql+mysqlconnector://poteto1212:Makt0112pc-49466@mariadb:3306/app-data"
+#環境変数化
+url = os.environ['MYSQL_URL']
 engin = create_engine(url)
